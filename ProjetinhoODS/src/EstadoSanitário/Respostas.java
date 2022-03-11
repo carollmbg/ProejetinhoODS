f00126c163da6eb2564fb03a649539776ced5d81
Caroline R <carol.lmbg25@gmail.com>
2022-03-11 18:46:52 -0300

    ajustes e atualizações, tentei estruturar as clases

--- ProjetinhoODS/src/EstadoSanitário/Respostas.java
+++ ProjetinhoODS/src/EstadoSanitário/Respostas.java
@@ -3,35 +3,34 @@ package EstadoSanitário;
 public class Respostas extends Perguntas {
 	
 	
-	public void Perguntas(String nome, String cpf, String cidade, int n_moradores, String problema_agua, 
-			String cheiro, String alaga, int  lixo, String rua, String coleta)
+	
+	@Override
+	public Respostas(String nome, String cpf, String cidade, int n_moradores, String problema_agua, 
+			String cheiro, String alaga, String  lixo, String rua, String coleta)
 	{
 
 		this.nome = nome;
 		this.cpf = cpf;
 		this.cidade = cidade;
-		this.n_moradores = n_moradores;
-		this.problema_agua = problema_agua;
-		this.cheiro = cheiro;
-		this.alaga = alaga;
-		this.nome = lixo;
-		this.nome = rua;
-		this.nome = coleta;
+		this.n_moradores = resposta1;
+		this.problema_agua = resposta2;
+		this.cheiro = resposta4;
+		this.alaga = resposta5;
+		this.nome = resposta6;
+		this.nome = resposta7;
+		this.nome = resposta8;
 	}
+	
+	
+	
+}
+	
+	
 		
 
-}
-
-
-switch(op)
-{
-case 1:
-	System.out.println("\nEncanado para o rio");
-case 2:
-	System.out.println("\nFossa séptica");
-case 3:
-	System.out.println("\nEncanado para estação de tratamento de esgoto");
-case 4:
-	System.out.println("\nfossa negra");
-case 5:
-	System.out.println("\nfossa negra");
+	
+	
+	
+	
+	
+	
